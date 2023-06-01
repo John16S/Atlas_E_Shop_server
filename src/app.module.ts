86065d2c89a9f25ea028d66b07502577f578046d
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-06-01 19:07:07 +0300

    Подключение к БД, Логика создание USER

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeConfigService } from './config/sequelizConfig.service';
+import { databaseConfig } from './config/configuration';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService, //настройки нашего БД передадим в useClass
+    }),
+
+    ConfigModule.forRoot({
+      //корневой config для nestjs
+      load: [databaseConfig], //наш databaseConfig из configuration.ts
+    }),
+    
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Header, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dto/create-user.dt';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    //*Пишем эндпоинты
+    constructor(private readonly userService: UsersService){}   //задаём UsersService чтоб использовать его методы
+
+    @Post('/signup')    //префикс "signup"  //*URL-(users/signup)
+    @HttpCode(HttpStatus.CREATED)   //будем возвращать код состояния HTTP 201 Created.
+    @Header('Content-type', 'application/json')   //устанавливает заголовок Content-Type со значением application/json
+
+    createUser(@Body() createUserDto: createUserDto){   //*Достаём данные из Body (отправляя с фронта)
+        return this.userService.create(createUserDto)   //*create- метод из класса UsersService
+    }
+}
