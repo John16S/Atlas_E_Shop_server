b4e8166b3851501487d87fb0670adbfb14eb5557
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-06-03 18:50:23 +0300

    Jest tests для Users, Auth модулей

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeConfigService } from './config/sequelizConfig.service';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
 import { databaseConfig } from './config/configuration';
 import { AuthModule } from './auth/auth.module';
 import { GoodsModule } from './goods/goods.module';
